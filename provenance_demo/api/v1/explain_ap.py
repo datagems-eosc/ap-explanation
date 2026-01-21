e7891359576eaf3e726cc2c083e7c5b566b6ce85
SoTrx <11771975+SoTrx@users.noreply.github.com>
2026-01-21 15:08:54 +0000

    fix: Adjusting explain_ap method to new ap format

--- provenance_demo/api/v1/explain_ap.py
+++ provenance_demo/api/v1/explain_ap.py
@@ -13,32 +13,25 @@ logger = getLogger(__name__)
 
 
 async def explain_ap(ap: PgJson, prov_svc: ProvenanceService = Depends(get_provenance_service)):
-    # TODO: Parse AP
-    try:
-        sql_nodes = ap.get_nodes_by_label("SQL_Operator")
-        match len(sql_nodes):
-            case 0:
-                raise HTTPException(
-                    status.HTTP_422_UNPROCESSABLE_CONTENT,
-                    detail="This AP has no SQL Operators !"
-                )
-            case n if n > 1:
-                logger.warning(
-                    "Multiples SQL_OPERATOR nodes detected, only the first one will be processed")
-
-        # Check that the found SQL node is well formed
-        sql_node = sql_nodes[0]
-        if not sql_node.properties or "Query" not in sql_node.properties.keys():
+    sql_nodes = ap.get_nodes_by_label("Provenance_SQL_Operator")
+    match len(sql_nodes):
+        case 0:
             raise HTTPException(
                 status.HTTP_422_UNPROCESSABLE_CONTENT,
-                detail="Malformed AP : SQL operator has no Query property"
+                detail="This AP has no SQL Operators !"
             )
+        case n if n > 1:
+            logger.warning(
+                "Multiples SQL_OPERATOR nodes detected, only the first one will be processed")
 
-        prov = await prov_svc.compute_provenance(sql_node.properties["Query"])
-
-        return loads(prov or "[]")
-
-    except Exception as e:
-        logger.error("Failed to explain AP", exc_info=e)
+    # Check that the found SQL node is well formed
+    sql_node = sql_nodes[0]
+    if not sql_node.properties or "query" not in sql_node.properties.keys():
         raise HTTPException(
-            status_code=500, detail="Failed to compute provenance") from e
+            status.HTTP_422_UNPROCESSABLE_CONTENT,
+            detail="Malformed AP : SQL operator has no 'query' property"
+        )
+
+    prov = await prov_svc.compute_provenance(sql_node.properties["query"])
+
+    return loads(prov or "[]")
