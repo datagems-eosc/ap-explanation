a9f8509b79ad3caa3c2c3fffc58900f224446a24
SoTrx <11771975+SoTrx@users.noreply.github.com>
2026-01-26 13:53:26 +0000

    feat: Adding support for ROOT_PATH

--- ap_explanation/main.py
+++ ap_explanation/main.py
@@ -1,8 +1,10 @@
 import logging
+from os import getenv
+from pathlib import Path
 
 import uvicorn
-from dotenv import load_dotenv
 from fastapi import FastAPI
+from tomllib import loads as loads_toml
 
 from ap_explanation.api.v1.routes import router
 from ap_explanation.di import container_lifespan
@@ -10,13 +12,19 @@ from ap_explanation.di import container_lifespan
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-load_dotenv()
+# Retrieve current project version from toml
+pyproject = loads_toml(Path("pyproject.toml").read_text())
+project_version = pyproject["project"]["version"]
+
+ROOT_PATH = getenv("ROOT_PATH", "")
 
 app = FastAPI(
     title="AP Explanation API",
     description="API for explaining Analytical Patterns with provenance tracking using ProvSQL",
-    version="0.0.1",
+    version=project_version,
     lifespan=container_lifespan,
+    root_path=ROOT_PATH,
+
 )
 
 
@@ -24,7 +32,7 @@ app = FastAPI(
 def index():
     return {
         "service": "AP Explanation",
-        "version": "0.0.1"
+        "version": app.version
     }
 
 
